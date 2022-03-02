999a256418aa0c7b65cda33de51e043ae00c22b6
itdanja <itdanja@kakao.com>
2022-03-02 15:23:11 +0900

    day02

--- java1/src/Day02/Day02_5_문제2_문제14.java
+++ java1/src/Day02/Day02_5_문제2_문제14.java
@@ -52,11 +52,21 @@ public class Day02_5_문제2_문제14 {
 		System.out.println(" 입력한 정수는 홀수 판단 : " + ( 문제4%2 == 1 ) );
 		
 		//문제5 : 하나의 정수를 입력받아 7의 배수 이면서 짝수 인지 확인 [ true , false ]
+		System.out.print(" 문제5) 정수 입력 : "); 	int 문제5 = scanner.nextInt();
+		System.out.println("입력한 정수는 7의배수 이면서 짝수 판단 : " 
+							+ ( 문제5%7 == 0 && 문제5%2 == 0 ) );
 		
 		//문제6 : 두개의 정수를 입력받아 더 큰지 출력  [ true , false ]
+		System.out.print(" 문제6) 정수1 입력 : ");	int 문제6_1 = scanner.nextInt();
+		System.out.print(" 문제6) 정수2 입력 : ");	int 문제6_2 = scanner.nextInt();
+		
+		int 큰수 = 문제6_1 > 문제6_2 ? 문제6_1 : 문제6_2; 
+		System.out.println(" 더 큰 수는 : " + 큰수 );
 		
 		//문제7 : 반지름을 입력받아 원 넓이 출력하기 
 				// 원 넓이 공식 => 반지름 * 반지름 * 원주율[3.14]
+		System.out.print(" 문제7) 반지름 입력 : "); 	int 문제7 = scanner.nextInt();
+		System.out.println(" 원 넓이 : " + (문제7*문제7*3.14) );
 		
 		//문제8 : 두 실수를 입력받아 앞실수의 값이 뒤의 값의 몇% 인지 출력하기
 				// 예) 54.5   84.3 이면    64.285714%
