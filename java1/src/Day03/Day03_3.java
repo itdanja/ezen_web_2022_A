238fb6a38b4d3b87b1bb885b9754950f2f8f1435
itdanja <itdanja@kakao.com>
2022-03-03 12:20:38 +0900

    day03

--- java1/src/Day03/Day03_3.java
+++ java1/src/Day03/Day03_3.java
@@ -2,6 +2,9 @@ package Day03;
 
 import java.util.Scanner;
 
+	// 범위 주석처리 : 범위지정 + ctrl + / 
+	//				범위지정 + ctrl+shift + /
+
 public class Day03_3 {
 	
 	public static void main(String[] args) {
@@ -9,20 +12,20 @@ public class Day03_3 {
 		Scanner scanner = new Scanner(System.in);
 		//문제6
 		// 점수를 입력받아 점수가 90점 이상이면 합격 아니면 불합격 출력 
-		System.out.print(" 점수 : "); int 점수 = scanner.nextInt();
-		if( 점수 >= 90 ) System.out.println(" 합격. ");
-		else System.out.println(" 불합격. ");
+//		System.out.print(" 점수 : "); int 점수 = scanner.nextInt();
+//		if( 점수 >= 90 ) System.out.println(" 합격. ");
+//		else System.out.println(" 불합격. ");
 		
 		//문제7
 		// 점수를 입력받아 점수가 90점 이상이면 A등급
 		//				점수가 80점 이상이면 B등급
 		//				점수가 70점 이상이면 C등급
 		//				그외 재시험
-		System.out.print(" 점수 : "); int 점수2 = scanner.nextInt();
-		if( 점수2 >= 90 )System.out.println("A등급");
-		else if( 점수2 >=80 )System.out.println("B등급");
-		else if( 점수2 >=70 )System.out.println("C등급");
-		else System.out.println("재시험");
+//		System.out.print(" 점수 : "); int 점수2 = scanner.nextInt();
+//		if( 점수2 >= 90 )System.out.println("A등급");
+//		else if( 점수2 >=80 )System.out.println("B등급");
+//		else if( 점수2 >=70 )System.out.println("C등급");
+//		else System.out.println("재시험");
 		
 		//문제8 로그인페이지
 		// [ 입력 ] : 아이디와 비밀번호를 입력받기
@@ -30,6 +33,37 @@ public class Day03_3 {
 		//           비밀번호가 1234 일 경우에는 
 		//          로그인 성공 아니면 로그인 실패 출력
 		
+		System.out.println("---------로그인 페이지-----------");
+		System.out.print(" 아이디 : ");	String id = scanner.next();
+			// char 자료형 [ 문자 1개 저장 ]
+			// 자료형으로 문자열을 저장할수 없다.
+				// 1. String 클래스 사용하면 문자열 저장가능 
+				// 2. char 배열/리스트 사용하면 가능 
+		System.out.print(" 비밀번호 : ");	int pw = scanner.nextInt();
+			// 객체는 연산불가
+				// 자료형 사용하는 변수는 연산자 사용 가능 
+				// 클래스 사용하는 객체는 연산자 사용 불가 -> 메소드
+					// 문자열 비교시 .equals
+					// 문자열1.equals( 문자열2 ) 
+		boolean 로그인성공 = false;
+			// boolean : 1비트 true 혹은 false 저장 
+		if( id.equals("admin") ) {	// id 는 클래스 객체이기 때문에 == 불가능 // 객체비교시에는 .equals() 사용
+			// 입력한 id가 admin 이면 
+			if( pw == 1234 ) { // pw 는 자료형 변수이기 때문에 == 가능  
+				// 입력한 pw 1234 이면 
+				System.out.println(" 알림) 로그인 성공 ");
+				로그인성공 = true; // id , pw 가 모두 동일하면 변수값 변경 [
+			}
+			else { // 입력한 pw 1234 아니면 
+				System.out.println(" 알림) 비밀번호 틀렸습니다. ");
+			}
+		}else { // 입력한 id가 admin 이 아니면 
+			System.out.println(" 알림) 아이디가 틀렸습니다. ");
+		}
+		
+		if( 로그인성공 == false ) System.out.println(" 알림) 로그인 실패 ");
+			// 로그인 성공 변수가 false 이면 출력 
+	
 		/* 문제9
 			[ 입력 ] : 국어,영어,수학 입력받기
 			[ 조건 ]
