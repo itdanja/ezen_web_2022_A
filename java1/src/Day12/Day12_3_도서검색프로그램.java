d2f86a132f4ba86c2496536a425c0932f9f60be4
itdanja <itdanja@kakao.com>
2022-03-17 16:26:53 +0900

    day12

--- java1/src/Day12/Day12_3_도서검색프로그램.java
+++ java1/src/Day12/Day12_3_도서검색프로그램.java
@@ -1,8 +1,11 @@
 package Day12;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Day12_3_도서검색프로그램 {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		// 도서 검색 프로그램
 			// [조건]
@@ -21,7 +24,31 @@ public class Day12_3_도서검색프로그램 {
 					// 예시) '언어' 와 Language 입력 -> 선택된 도서명의 수정할 문자와 새로운 문자 입력받아 ->  수정 완료 
 						// 혼자 공부하는 C 언어 ->  혼자 공부하는 C Language 
 		
+		
+		String[] 도서목록 = {   "된다! 네이버 블로그&포스트","스프링 부트 실전 활용 마스터",
+				"Tucker의 Go 언어 프로그래밍","혼자 공부하는 C 언어" };
+		
+		while( true ) {
+			System.out.println("1.검색 2.수정");
+			byte[] bytes = new byte[100];
+			int count =  System.in.read(bytes); // 키보드로부터 [스트림] 바이트를 읽어오기 // 일반예외!!!! [ 예외 떠넘기기 ]
+			// 읽어온 바이트개수 
 			
+			String ch = new String( bytes , 0 , count-2 ); // 바이트배열 --> 문자열 변환 [ -2 = \n\r 제거 ]
+			
+			if( ch.equals("1") ) { 
+				System.out.println(" *** 검색 *** ");
+				
+			}else if( ch.equals("2") ) {
+				System.out.println(" *** 수정 *** ");
+				
+			}
+			
+		}
+		
+		
+		
+		
 		
 		
 	}
