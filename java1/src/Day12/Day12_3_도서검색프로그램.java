6221dbec97e8e852f945eb3469683ae9e05ab449
itdanja <itdanja@kakao.com>
2022-03-17 16:50:21 +0900

    day12

--- java1/src/Day12/Day12_3_도서검색프로그램.java
+++ java1/src/Day12/Day12_3_도서검색프로그램.java
@@ -29,7 +29,7 @@ public class Day12_3_도서검색프로그램 {
 				"Tucker의 Go 언어 프로그래밍","혼자 공부하는 C 언어" };
 		
 		while( true ) {
-			System.out.println("\n\n 1.검색 2.수정");
+			System.out.print("\n\n 1.검색 2.수정");
 			byte[] bytes = new byte[100];
 			int count =  System.in.read(bytes); // 키보드로부터 [스트림] 바이트를 읽어오기 // 일반예외!!!! [ 예외 떠넘기기 ]
 			// count 변수 : 읽어온 바이트개수 
@@ -37,7 +37,7 @@ public class Day12_3_도서검색프로그램 {
 			
 			if( ch.equals("1") ) { 
 				System.out.println(" *** 검색 *** ");
-				System.out.println(" 검색 : "); count = System.in.read( bytes );
+				System.out.print(" 검색 : "); count = System.in.read( bytes );
 				String search = new String( bytes , 0 , count-2 ); // 찾을문자 
 				
 				System.out.println(" *** 검색 결과 *** ");
@@ -56,7 +56,33 @@ public class Day12_3_도서검색프로그램 {
 				
 			}else if( ch.equals("2") ) {
 				System.out.println(" *** 수정 *** ");
+				System.out.print(" 수정할 도서명[일부] : ");
+				count = System.in.read( bytes );	// 키보드로부터 바이트 읽어와서 배열에 저장 
+				String search = new String(bytes,0,count-2); // 0번인덱스 바이트부터 -2제외한 인덱스까지
+				int i = 0; // 인덱스 
+				for( String temp : 도서목록 ) {
+					if( temp.contains(search) ) {	
+						// 문자열.contains("찾을문자") : 문자열내 찾을문자가 true[있다] / false[없다] 반환
+						System.out.println( "도서번호 : "+ i+"  교재명 : "+ temp );
+					}
+					i++;
+				}
+				// 도서번호 [ 인덱스 ] 입력받아 수정하기
+				System.out.print(" 도서번호 선택 : ");		count = System.in.read( bytes );
+					String ch2 = new String( bytes , 0 , count-2 );
+					int index = Integer.parseInt(ch2); 	// ch2 문자열로 입력받음 ---> 인덱스[숫자] 변환
+					
+				// 수정 내용 입력받기 
+				System.out.println(" 기존 문자 : ");
+					count = System.in.read( bytes );
+					String oldstr = new String( bytes , 0 , count-2);
+				System.out.println(" 새로운 문자 : ");   	count = System.in.read(bytes);
+					String newstr = new String( bytes , 0 , count-2);
+					
+				// 수정처리 
+				도서목록[index].replace(oldstr, newstr);
 				
+				System.out.println(" 변경 되었습니다. ");
 			}
 			
 		}
