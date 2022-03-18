2f8b0dd5c7b9b93c43b3309828ace4b4dededc87
itdanja <itdanja@kakao.com>
2022-03-18 12:24:28 +0900

    day13

--- java1/src/Day13/Day13_3_달력.java
+++ java1/src/Day13/Day13_3_달력.java
@@ -20,7 +20,7 @@ public class Day13_3_달력 {
 		while( true ) { // 무한루프 [ 종료조건 : 4번 ] 
 			// 1. 캘린더 클래스내 현재 날짜/시간 객체 호출 
 			Calendar calendar = Calendar.getInstance(); //예) 2022년3월18일
-			// * 해당 월의 1일의 요일찾기 
+			// 2. * 해당 월의 1일의 요일찾기 
 				calendar.set( year , month-1 , 1 ); // 예) 2022년3월1일 // 1. 사용자 정의 캘린터 설정 
 				int sweek = calendar.get( Calendar.DAY_OF_WEEK ); // 2. 3월1일의 요일
 				int eday = calendar.getActualMaximum( Calendar.DAY_OF_MONTH ); // 3. 3월의 마지막 일 
@@ -28,17 +28,17 @@ public class Day13_3_달력 {
 			System.out.println("\n\n********************** "+year+"년" + month+"월 ******************");
 			System.out.println("일\t월\t화\t수\t목\t금\t토");
 			
-			// * 현재 월 1일의 위치 앞에 공백 채우기 
+			// 3-1.* 현재 월 1일의 위치 앞에 공백 채우기 
 			for( int i = 1 ; i < sweek ; i++ ) { // 3월1일 = 화 = 3 
 				System.out.print("\t"); // 공백출력
 			}
-			// 1일부터 마지막날짜 까지 출력 
+			// 3-2. 1일부터 마지막날짜 까지 출력 
 			for( int i = 1 ; i<=eday ; i++ ) {
 				System.out.print(i+"\t");  // i = 일수 출력
 				if( sweek % 7 == 0 ) System.out.println(); // 토요일마다 줄바꿈처리 [ 토요일=7 ] 요일이 7배수마다
 				sweek++;  // 하루씩 요일 증가 
 			}
-			// 버튼 [ 1.◀(이전달)  2.▶(다음달) 3.다시검색 4.종료 ]
+			// 4. 버튼 [ 1.◀(이전달)  2.▶(다음달) 3.다시검색 4.종료 ]
 			try { // 만약에 try { } 안에서 예외[에러]가 발생하면 catch 이동 ( 프로그램 다운x)
 				System.out.print(" \n\n *[ 1.◀(이전달)  2.▶(다음달) 3.다시검색 4.종료 선택 :  ");
 				Scanner scanner = new Scanner( System.in ); int ch = scanner.nextInt();
