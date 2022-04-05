0ff3aec1e6a33421ef6a5fddacfdcb8648ea0ee3
MSI <kgs2072@naver.com>
2022-04-05 17:42:36 +0900

    day24

--- java2/src/app/Start.java
+++ java2/src/app/Start.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Start extends Application {
@@ -29,10 +30,16 @@ public class Start extends Application {
 				// 상대경로 : 현 위치[프로젝트기준 src] 기준 경로 
 					//  생략 [ src폴더부터 ]  /img/파일명.확장자
 			stage.getIcons().add(image); // 3. 스테이지 아이콘설정 
+			
+		// * 외부폰트 설정
+			// 1. 폰트 로드
+			Font.loadFont( getClass().getResourceAsStream("SANGJU Gotgam.ttf"), 15);
+			// 2. 현대 scene에 외부 스타일시트 적용
+			scene.getStylesheets().add( getClass().getResource("application.css").toExternalForm() );
+			
 		stage.setResizable(false); // 4. 스테이지 크기 변경 불가 
 		stage.setTitle("이젠마켓"); // 2.스테이지 창 이름
 		stage.show(); // 1. 스테이지 열기 
-		
 	}
 	public static void main(String[] args) {
 		
@@ -40,3 +47,11 @@ public class Start extends Application {
 	}
 	
 }
+
+
+
+
+
+
+
+
