fcdf808507b4aa074addd298c6d49b80be312890
MSI <kgs2072@naver.com>
2022-04-06 12:09:32 +0900

    day25

--- java2/src/controller/product/Productadd.java
+++ java2/src/controller/product/Productadd.java
@@ -1,5 +1,6 @@
 package controller.product;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -10,13 +11,19 @@ import dto.Product;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
 
 public class Productadd implements Initializable {
 
@@ -76,28 +83,47 @@ public class Productadd implements Initializable {
     		if( opt4.isSelected() ) { pcategory ="생활용품";}
     	int mnum = Login.member.getMnum();
     	// 2. 객체화
-    	Product product = new Product(0, pname, null, pcontent, pcategory, pprice, 1, null, mnum);
+    	Product product = new Product(0, pname, pimage, pcontent, pcategory, pprice, 1, null, mnum);
     	// 3. DB처리
     	boolean result =  ProductDao.productDao.add(product);
-    	// 4. 결과처리
     	if( result ) {
+    		Alert alert = new Alert(AlertType.INFORMATION ); // 메시지 
+    			alert.setHeaderText("제품 등록 완료");
+    		alert.showAndWait();	
+    		Home.home.loadpage("/view/product/product.fxml"); // 화면전화
     		
     	}else {
-    		
+    		Alert alert = new Alert(AlertType.INFORMATION );
+				alert.setHeaderText("제품 등록 실패 [ 관리자에게 문의]");
+			alert.showAndWait();
     	}
-    	
     }
-
+    private String pimage = null; // 메소드 밖에서 선언하는 이유  : imgadd메소드와 add 메소드 에서 사용하기 위해
+    
+    @FXML
+    void imgadd(ActionEvent event) { // 이미지등록 버튼 클릭했을때
+    	// 1. 파일 선택 클래스 
+    	FileChooser fileChooser = new FileChooser();
+    	// 2. *파일 선택[필터] 형식 
+    		fileChooser.getExtensionFilters().add( 
+    				new ExtensionFilter("이미지파일:image file" , "*png" , "*jpeg" , "*jpg","*gif") );
+    	// 3. 새로운 stage에서 파일선택 화면 실행 
+    	File file = fileChooser.showOpenDialog( new Stage() );
+    		// 파일선택객체.showOpenDialog( 스테이지 ) ;
+    		// fileChooser 화면에서 선택된 파일을 file 클래스객체화
+    	// 4. 선택한 파일의 경로 표시
+    	txtpath.setText("파일 경로 : " + file.getPath() ); // 경로 구분선 : \
+    	// 5. 파일경로 
+    	pimage = file.toURI().toString(); // 경로 구분선 : /
+    	// 6. 미리보기 이미지컨트롤에 띄우기
+    	Image image = new Image( pimage ); // 해당 이미지의 경로 구분 이  / 구분 되어 있어야함 
+    	img.setImage(image); // ImageView 에 해당 이미지 넣어주기
+    }
+    
     @FXML
     void back(ActionEvent event) {
     	Home.home.loadpage("/view/product/product.fxml");
     }
-
-    @FXML
-    void imgadd(ActionEvent event) {
-
-    }
-	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 	}
