bd756d40b0aaa8f4775dedde06a0e4f47ea2379d
MSI <kgs2072@naver.com>
2022-04-06 11:21:07 +0900

    day25

--- java2/src/controller/product/Productadd.java
+++ java2/src/controller/product/Productadd.java
@@ -3,6 +3,10 @@ package controller.product;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import controller.home.Home;
+import controller.login.Login;
+import dao.ProductDao;
+import dto.Product;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -58,12 +62,35 @@ public class Productadd implements Initializable {
 
     @FXML
     void add(ActionEvent event) {
-
+    	// 1. 컨트롤에 입력된 데이터 가져오기
+    	String pname = txtpname.getText();
+    	String pcontent = txtpcontent.getText();
+    	int pprice = Integer.parseInt( txtpprice.getText() ) ;	// 문자열 -> 정수열 변환 [ Integer.parseInt ]
+    		// * 카테고리
+    	String pcategory = null;
+    		if( opt1.isSelected() ) { // 만약에 opt1라는 fxid의 컨트롤(라디오버튼)이 선택되었으면
+    			pcategory="남성의류";
+    		}
+    		if( opt2.isSelected() ) { pcategory ="여성의류";}
+    		if( opt3.isSelected() ) { pcategory ="게임기기";}
+    		if( opt4.isSelected() ) { pcategory ="생활용품";}
+    	int mnum = Login.member.getMnum();
+    	// 2. 객체화
+    	Product product = new Product(0, pname, null, pcontent, pcategory, pprice, 1, null, mnum);
+    	// 3. DB처리
+    	boolean result =  ProductDao.productDao.add(product);
+    	// 4. 결과처리
+    	if( result ) {
+    		
+    	}else {
+    		
+    	}
+    	
     }
 
     @FXML
     void back(ActionEvent event) {
-
+    	Home.home.loadpage("/view/product/product.fxml");
     }
 
     @FXML
@@ -73,7 +100,13 @@ public class Productadd implements Initializable {
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
-		
 	}
 }
+
+
+
+
+
+
+
+
--- java2/src/dao/ProductDao.java
+++ java2/src/dao/ProductDao.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import dto.Product;
+
 public class ProductDao {
 	
 	private Connection con;
@@ -21,4 +23,48 @@ public class ProductDao {
 		}catch(Exception e ) {}
 	}
 	
+	// 1. 力前 殿废 
+	public boolean add( Product product ) {
+		try {
+			String sql = "insert into product(pname,pimg,pcontent,pcategory,pprice,pactivation,mnum) values(?,?,?,?,?,?,?)";
+			ps = con.prepareStatement(sql);
+			ps.setString( 1 , product.getPname() );
+			ps.setString( 2 , product.getPimg() );
+			ps.setString( 3 , product.getPcontent() );
+			ps.setString( 4 , product.getPcategory() );
+			ps.setInt( 5 , product.getPprice() );
+			ps.setInt( 6 , product.getPactivation() );
+			ps.setInt( 7 , product.getMnum() );
+			ps.executeUpdate();
+			return true;
+		}catch(Exception e ) { System.out.println( "[SQL 坷幅]"+e  ); }
+		return false;
+	}
+	// 2. 葛电 力前 免仿 
+	
+	// 3. 力前 炼雀
+	
+	// 4. 力前 昏力
+	
+	// 5. 力前 荐沥 
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
