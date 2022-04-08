e22855431cb0af23efabc3bd2040cf397523baaf
itdanja <itdanja@kakao.com>
2022-04-08 11:25:48 +0900

    day27

--- java2/src/controller/Chatting.java
+++ java2/src/controller/Chatting.java
@@ -12,6 +12,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
@@ -30,13 +32,29 @@ public class Chatting implements Initializable {
     private Button btnsend;
 
     @FXML
-    private TextField txtname;
+    private TableView<?> roomtable;
 
     @FXML
-    private TextField txtip;
+    private TextField txtroomname;
 
     @FXML
-    private TextField txtport;
+    private Button btnadd;
+
+    @FXML
+    private TextArea txtmidlist;
+
+    @FXML
+    private Label lblselect;
+
+    @FXML
+    void add(ActionEvent event) {
+
+    }
+
+    @FXML
+    void msg(ActionEvent event) {
+
+    }
     
     Socket socket;   // 1. 클라이언트 소켓 선언 
     
