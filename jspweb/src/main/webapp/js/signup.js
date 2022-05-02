0b7e125b27ce3cfb5853e69b00879e038988ad5c
itdanja <itdanja@kakao.com>
2022-05-02 14:59:04 +0900

    day43_signup

--- jspweb/src/main/webapp/js/signup.js
+++ jspweb/src/main/webapp/js/signup.js
@@ -3,8 +3,7 @@
 // $( function() { 실행문; } ) : 문서 실행시 무조건 실행되는 함수
 	// js식		id불러오기  .getElementById( id명 )
 	// jquery식	id불러오기  $("#id명")
-		// keyup(  ) : 해당 id에 키보드가 눌렸을때 [ 입력 되었을때 ] 
-		
+		// keyup(  ) : 해당 id에 키보드가 눌렸을때 [ 입력 되었을때 ]
 $( function(){  // 문서 열리면 해당 코드가 실행 
 	// 아이디 체크
 	$("#mid").keyup( function(){	// mid 가 입력될때마다 해당 함수 실행
@@ -48,7 +47,108 @@ $( function(){  // 문서 열리면 해당 코드가 실행
 		}
 	}); // keyup end 
 	
+	// 비밀번호확인 체크 
+	$("#mpasswordcheck").keyup( function(){  // 비밀번호 입력할때마다
+		// let mpassword = document.getElementById("mpassword").value;  // js식
+		let mpassword = $("#mpassword").val();  // jquery 식
+		let mpasswordcheck = $("#mpasswordcheck").val();
+		
+		let passswordj = /^[a-zA-Z0-9]{5,15}$/; // 정규표현식
+		
+		if( passswordj.test( mpasswordcheck ) ){ // 정규표현식 같으면
+			if( mpassword != mpasswordcheck ){ // 비밀번호 와 비밀번호체크 와 다르면
+				// equals(x)  //  != ( o )
+			$("#passwordchack").html("패스워드가 서로 다릅니다.");
+			}else{
+				$("#passwordchack").html("사용 가능한 비밀번호 입니다.");
+			}
+		}else{ // 정규현식 다르면
+			$("#passwordchack").html("영소문자 5~15 사이로 입력해주세요!");
+		}
+	}); // keyup end 
+	
+	// 이름 체크
+	$("#mname").keyup( function(){ 
+		let mname = $("#mname").val(); // 해당 id의 데이터 가져오기
+		let namej = /^[가-힣]{2,10}$/;	// 한글만 2~10 정규표현식
+		if( namej.test(mname) ){
+			$("#namechack").html( "사용가능한 이름입니다." );
+		}else{
+			$("#namechack").html( "한글 2~10 사이만 가능합니다." );
+		}
+	 }); // keyup end 
+	 
+	// 전화번호 체크 
+	$("#mphone").keyup( function(){ 
+		let mphone = $("#mphone").val();
+		let phonej = /^([0-9]{2,3})-([0-9]{3,4})-([0-9]{3,4})$/;
+		if( phonej.test(mphone) ){
+			$("#phonechack").html( "사용가능한 번호 입니다." );
+		}else{
+			$("#phonechack").html( "지역번호-0000-0000 형식으로 입력해주세요." );
+		}
+	}); // keyup end 
+	 
+	
+	
 }); // 문서 열리면 해당 코드가 종료 
+
+
+// 다음 api js
+  function sample4_execDaumPostcode() {
+        new daum.Postcode({
+            oncomplete: function(data) {
+                // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+                // 도로명 주소의 노출 규칙에 따라 주소를 표시한다.
+                // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+                var roadAddr = data.roadAddress; // 도로명 주소 변수
+                var extraRoadAddr = ''; // 참고 항목 변수
+
+                // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+                // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+                if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                    extraRoadAddr += data.bname;
+                }
+                // 건물명이 있고, 공동주택일 경우 추가한다.
+                if(data.buildingName !== '' && data.apartment === 'Y'){
+                   extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                }
+                // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+                if(extraRoadAddr !== ''){
+                    extraRoadAddr = ' (' + extraRoadAddr + ')';
+                }
+
+                // 우편번호와 주소 정보를 해당 필드에 넣는다.
+                document.getElementById('sample4_postcode').value = data.zonecode;
+                document.getElementById("sample4_roadAddress").value = roadAddr;
+                document.getElementById("sample4_jibunAddress").value = data.jibunAddress;
+                
+                // 참고항목 문자열이 있을 경우 해당 필드에 넣는다.
+                if(roadAddr !== ''){
+                    document.getElementById("sample4_extraAddress").value = extraRoadAddr;
+                } else {
+                    document.getElementById("sample4_extraAddress").value = '';
+                }
+
+                var guideTextBox = document.getElementById("guide");
+                // 사용자가 '선택 안함'을 클릭한 경우, 예상 주소라는 표시를 해준다.
+                if(data.autoRoadAddress) {
+                    var expRoadAddr = data.autoRoadAddress + extraRoadAddr;
+                    guideTextBox.innerHTML = '(예상 도로명 주소 : ' + expRoadAddr + ')';
+                    guideTextBox.style.display = 'block';
+
+                } else if(data.autoJibunAddress) {
+                    var expJibunAddr = data.autoJibunAddress;
+                    guideTextBox.innerHTML = '(예상 지번 주소 : ' + expJibunAddr + ')';
+                    guideTextBox.style.display = 'block';
+                } else {
+                    guideTextBox.innerHTML = '';
+                    guideTextBox.style.display = 'none';
+                }
+            }
+        }).open();
+    }
 
 
 
--- jspweb/src/main/webapp/member/signup.jsp
+++ jspweb/src/main/webapp/member/signup.jsp
@@ -18,17 +18,26 @@
 		<span id="passwordchack"></span>
 		<br>
 		이름 <input type="text" id="mname" placeholder="user name">
+		<span id="namechack"></span>
 		<br>
-		전화번호 : <input type="text" id="mphone" placeholder="user phone">
+		연락처 : <input type="text" id="mphone" placeholder="user phone">
+		<span id="phonechack"></span>
 		<br>
 		이메일 : <input type="text" id="memail" placeholder="user email">
 		<br>
-		주소 : <input type="text" id="maddress" placeholder="user address">
+			<input type="text" id="sample4_postcode" placeholder="우편번호">
+			<input type="button" onclick="sample4_execDaumPostcode()" value="우편번호 찾기"><br>
+			<input type="text" id="sample4_roadAddress" placeholder="도로명주소">
+			<input type="text" id="sample4_jibunAddress" placeholder="지번주소">
+			<span id="guide" style="color:#999;display:none"></span>
+			<input type="text" id="sample4_detailAddress" placeholder="상세주소">
+			<input type="text" id="sample4_extraAddress" placeholder="참고항목">
 		<br>
+		
 		<button onclick="signup()" type="button">가입하기</button>
 	</form>
 </div>
-
+<script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
 <script src="../js/signup.js" type="text/javascript"></script>
 <%@include file = "../footer.jsp" %>
 
