f97129c16b5d2666e28e335812bbd0b7df4cb8db
itdanja <itdanja@kakao.com>
2022-05-06 14:47:28 +0900

    day46 _ realpath upload

--- jspweb/src/main/java/controller/board/write.java
+++ jspweb/src/main/java/controller/board/write.java
@@ -40,21 +40,24 @@ public class write extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		// 저장 경로 [    \ : 제어문자  ->  경로 사용시 \\ , / ]
-		String uploadpath = "C:/Users/505-t/git/ezen_web_2022_A/jspweb/src/main/webapp/board/upload";
-		// 첨부파일 업로드 
+			// 1. 개발중인 프로젝트 경로 
+		// String uploadpath = "C:/Users/505-t/git/ezen_web_2022_A/jspweb/src/main/webapp/board/upload";
+			// 2. 서버 폴더 경로 
+				// 서버 경로 찾기 : request.getSession().getServletContext().getRealPath( 경로 ) ;
+		String uploadpath = request.getSession().getServletContext().getRealPath("/board/upload") ;
+
+		// 첨부파일 업로드 [ MultipartRequest : cos 라이브러리 제공 클래스 ] 
 		MultipartRequest multi = new MultipartRequest(
-				request ,		// 요청방식 
-				uploadpath , 	// 파일 저장 경로 
-				1024*1024*10 ,	// 파일 최대 용량 허용 범위
-				"UTF-8" ,		// 인코딩타입 
-				new DefaultFileRenamePolicy() 	// 동일한 파일명이 있을경우 자동 이름 변환 
-				);
-			
+				request ,		// 1. 요청방식 
+				uploadpath , 	// 2. 파일 저장 경로 
+				1024*1024*10 ,	// 3. 파일 최대 용량 허용 범위 [ 10MB ] 
+				"UTF-8" ,		// 4. 인코딩타입 
+				new DefaultFileRenamePolicy() 	// 4. 보안방식 : 동일한 파일명이 있을경우 자동 이름 변환 
+				);	
 		// 데이터 요청 
-		request.setCharacterEncoding("UTF-8");
 		String btitle = multi.getParameter("btitle");
 		String bcontent = multi.getParameter("bcontent");
-		String bfile = multi.getParameter("bfile");
+		String bfile = multi.getFilesystemName("bfile"); // 첨부파일 : getFilesystemName
 		
 			HttpSession session = request.getSession();
 			String mid = (String)session.getAttribute("login");
