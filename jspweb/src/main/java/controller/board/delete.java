29d9382d1a4f9671ca68e09c2b7954c49d750d24
itdanja <itdanja@kakao.com>
2022-05-09 12:23:48 +0900

    day47_board삭제시 실제파일도 서버에서 삭제

--- jspweb/src/main/java/controller/board/delete.java
+++ jspweb/src/main/java/controller/board/delete.java
@@ -1,5 +1,6 @@
 package controller.board;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import dao.BoardDao;
+import dto.Board;
 
 /**
  * Servlet implementation class delete2
@@ -32,12 +34,23 @@ public class delete extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		int bno =  Integer.parseInt( request.getParameter("bno") ) ;
-		boolean result =  BoardDao.getBoardDao().delete(bno);
+		
+		// * DB 삭제 전에 파일명 가져오기
+		Board board = BoardDao.getBoardDao().getboard(bno);
+		String bfile = board.getBfile();
+		
+		boolean result =  BoardDao.getBoardDao().delete(bno); // DB 삭제처리 
+		
 		// * java에서 html 작성하기(js 명령어) 사용하기
 		response.setCharacterEncoding("UTF-8");	// 응답 인코딩 타입 = 한글 
 		response.setContentType("text/html; charset=UTF-8"); // 응답 파일타입 = HTML 
 		PrintWriter out = response.getWriter();	// HTML 내보내기 메소드 사용
 		if( result ) {
+			// * 삭제 성공시 해당 파일도 서버에서 지우기 
+			String uploadpath = request.getSession().getServletContext().getRealPath("/board/upload/"+bfile ); // 파일 경로 찾기 
+			File file = new File(uploadpath); // 해당 파일 객체화 
+			file.delete(); // 파일 삭제하기 ( file클래스내 제공되는 delete() 메소드 = 파일삭제시 사용 )
+			
 			// 1. HTML 내보내기 
 //			out.println( "<html>");
 //			out.println( "<body>");
