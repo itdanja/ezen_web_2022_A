142b0740da3229f85aa84ed0ca5f4d75272aedf5
itdanja <itdanja@kakao.com>
2022-05-19 10:59:52 +0900

    day55 DB수정

--- jspweb/src/main/webapp/sql/product.sql
+++ jspweb/src/main/webapp/sql/product.sql
@@ -43,19 +43,18 @@ create table plike(
 create table porderdetail(
 	orderdetailno int primary key auto_increment , 	/* 주문상세 번호 */
     orderdetailactive  int ,   /* 주문상세 상태 */
-	sno int , 				/* 재고번호 */
     samount int , 			/* 수량 */ 
     totalprice	int , 		/* 총결제액 */
 	orderno int ,  			/* 주문번호 */ 
+	sno int , 				/* 재고번호 */
     foreign key( sno ) references stock( sno ) on update cascade , 
     foreign key( orderno ) references porder( orderno) on update cascade 
 );
 create table cart(
 	cartno int primary key auto_increment , 	/* 카트 번호 */
     samount int , 			/* 수량 */ 
-    sno int , 				/* 재고번호 */
     totalprice	int , 		/* 총결제액 */
-    pno int ,				/* 제품번호 */
+    sno int , 				/* 재고번호 */
 	mno int , 				/* 회원번호 */ 
     foreign key( sno ) references stock( sno ) on update cascade , 
     foreign key ( mno ) references member(mno) on update cascade 
