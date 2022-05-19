056a006c1dfb285e8f34365e76cd85175c876aa3
itdanja <itdanja@kakao.com>
2022-05-19 14:35:04 +0900

    day55 서블릿 -> js [ json통신 ]

--- jspweb/src/main/java/controller/product/getcart.java
+++ jspweb/src/main/java/controller/product/getcart.java
@@ -28,23 +28,31 @@ public class getcart extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String mid = (String)request.getSession().getAttribute("login");
 		int mno = MemberDao.getmemberDao().getmno(mid);
+		// DB처리 
 		JSONArray jsonArray = ProductDao.getProductDao().getcart(mno);
-		response.getWriter().print( jsonArray );
+		// request , response -> 전송 인코딩 타입 -> 문자열 
+			// 1. 응답객체내 한글 인코딩 타입설정 
+			response.setCharacterEncoding("UTF-8");
+			// *** 2. 응답객체의 자료형 [ 문자열 -> json 형식 ] 
+			response.setContentType("application/json");
+			// 3. 응답전송 
+			response.getWriter().print( jsonArray );
+			
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
 }
+
+
+
+
+
+
