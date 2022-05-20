36fece020a21c5fad094f5ea362f5e51cd014dde
itdanja <itdanja@kakao.com>
2022-05-20 10:34:37 +0900

    day56_장바구니페이지 뷰

--- jspweb/src/main/webapp/js/productcart.js
+++ jspweb/src/main/webapp/js/productcart.js
@@ -2,19 +2,51 @@ $( function(){
 	$.ajax({
 		url : 'getcart' ,
 		success : function( jsonarray ){
-			
 			// js : json 객체내 key값 이용한 value 추출 
 				// 객체명[key] -> value 호출 
 				// jsonarray[i][key] -> json배열내 i번째 객체의 key 값 호출 
-			let tr = "";
+			let tr = '<tr> <!-- 헤더 -->'+
+'					<th width="60%">상품정보</th> 	<!-- width속성 : 가로길이  -->'+
+'					<th width="20%">수량</th> 		'+
+'					<th width="20%">가격</th> '+
+'				</tr>';
 			for( let i = 0 ; i<jsonarray.length; i++ ){
 				tr += 
 				'<tr>'+
-					'<td width="5%"><img width="100%" alt="" src="../admin/productimg/'+jsonarray[i]['pimg']+'"></td>'+
-					'<td> '+jsonarray[i]['pname']+' <br>'+jsonarray[i]['scolor']+'/'+jsonarray[i]['ssize']+'</td>'+
-					'<td> '+jsonarray[i]['totalprice']+'  </td>'+
-					'<td> <button>X</button> </td>'+
-				'</tr>'
+'					<td> <!--  상품정보 열 -->'+
+'						<div class="row"> <!-- row(b) : 하위 태그를 가로배치   -->'+
+'							<div class="col-sm-2"> <!-- col-sm-2(b) : 12그리드 중 2그리드 사용 -->'+
+'								<img alt="" src="/jspweb/admin/productimg/'+jsonarray[i]["pimg"]+'" width="70%"> '+
+'							</div>'+
+'							<div class="col-sm-10">'+
+'								<div class="pnamebox"> '+jsonarray[i]["pname"]+' </div>'+
+'								<div class="optionbox">'+jsonarray[i]["scolor"]+' / '+jsonarray[i]["ssize"]+'</div>'+
+'							</div>'+
+'						</div>   '+
+'						<br>'+
+'					</td> '+
+'					<td class="align-middle"> <!-- 수량 --> <!-- align-middle(b) : 수직 중앙배치 -->'+
+'						<div class="row g-0"> <!-- g-0(b) : 그리드 간 여백 제거 -->'+
+'							'+
+'							<div class="col-sm-5 offset-3">'+
+'								<!-- form-control(b) : 입력상자 형식 꾸미기 -->'+
+'								<input readonly id="" value="'+jsonarray[i]["samount"]+'" type="text" class="form-control" style="background-color: white;">'+
+'							</div>'+
+'							'+
+'							<div class="col-sm-2">'+
+'								<button class="amount_btn"> ▲ </button>'+
+'								<button class="amount_btn"> ▼ </button>'+
+'							</div>'+
+'						</div>'+
+'					</td>'+
+'					<td class="align-middle"> <!--  가격 -->'+
+'						<div class="row g-0">'+
+'							<div class="col-md-8 pricebox">'+jsonarray[i]["totalprice"].toLocaleString()+'원</div>'+
+'							<div class="col-md-4 deletebox"><button>X</button></div>'+
+'						</div>'+
+'					</td>'+
+'				</tr>';
+
 			}
 			$("#carttable").html( tr );
 		}
--- jspweb/src/main/webapp/product/productcart.jsp
+++ jspweb/src/main/webapp/product/productcart.jsp
@@ -5,16 +5,25 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+
+	<link href="/jspweb/css/productcart.css" rel="stylesheet">
+
 </head>
 <body>
-
+	<!-- css에서 정의되지 않은 클래스명은 모두 부트스트랩  -->
+	
 	<%@include file = "../header.jsp" %>
-		<div class="container">
-			<table class="table" id=carttable>
+		<div class="container"> <!-- container(b) : 컨테이너  -->
+			<table class="table my-5" id="carttable"> <!-- table(b) : 테이블 // my(b) : 위아래 마진(바깥여백) -->
+				
 			</table>
 		</div>
 	<script type="text/javascript" src="/jspweb/js/productcart.js"></script>
 	<%@include file = "../footer.jsp" %>
+	
+	
+	
+	
 
 </body>
 </html>
