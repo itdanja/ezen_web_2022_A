ea7e8e9e152f800afb157b578866a77646e257cd
itdanja <itdanja@kakao.com>
2022-05-23 10:38:48 +0900

    day57

--- jspweb/src/main/webapp/js/orderpay.js
+++ jspweb/src/main/webapp/js/orderpay.js
@@ -1,21 +1,102 @@
+
 let jsonarray; // JSON 형식의 변수를 선언
+
+let sumprice = 0; // 상품 총가격 변수 선언 
+let deliverypay = 0; // 배송비 변수 선언 
 let totalpay = 0; // 총주문액 변수 선언 
+let point = 0; // 포인트 변수 선언 
+
 /* 1. 자바스크립트 열리면 무조건 실행되는 메소드 */
-$( function(){  
-	getcart();
+$( function(){  // $(document).ready( function(){});  // 문서내에서 대기상태 이벤트
+	
+	getcart();	// 제품 출력 메소드 불러오기
+	
+	// 받는사람 정보가 기존 회원과 동일 버튼눌렀을때
+	$("#checkbox").change( function(){	
+		// 체크박스가 변경 되었을때 이벤트 
+		if( $("#checkbox").is(":checked") ){
+			//만약에 체크박스가 체크가 되어 있으면
+			$("#ordername").val("유재석");
+			$("#orderphone").val("010-4444-4444");
+			$("#orderaddress").val("안산시 ㅋㅋㅋㅋㅋ");
+		}else{
+			$("#ordername").val("");
+			$("#orderphone").val("");
+			$("#orderaddress").val("");
+		}
+	});
+	
 });
+
 /* 2.  장바구니 데이터 호출 메소드 */
 function getcart(){
 	$.ajax({
 		url : 'getcart' ,
 		success : function( json ){
 			jsonarray = json;	/* 응답받은 데이터를 전역변수에 넣어주기 */
+			cartview();
+		}
+});
+}
+/* 3. js내 존재하는 json 객체를 table 에 데이터 출력  */
+function cartview(){
+			sumprice = 0; /*상품 총가격 */
+			deliverypay = 0; // 배송비 
+			totalpay = 0; // 총주문액 
+			
+			let tr = '<tr> <!-- 헤더 -->'+
+'					<th width="60%">상품정보</th>'+
+'					<th width="20%">수량</th> 		'+
+'					<th width="20%">가격</th> '+
+'				</tr>'
 			
 			for( let i = 0 ; i<jsonarray.length; i++ ){
-				totalpay += jsonarray[i]["totalprice"];
+				
+				sumprice += jsonarray[i]["totalprice"]; // 누적합계
+				
+				tr += 
+				'<tr>'+
+'					<td> <!--  상품정보 열 -->'+
+'						<div class="row"> <!-- row(b) : 하위 태그를 가로배치   -->'+
+'							<div class="col-sm-2"> <!-- col-sm-2(b) : 12그리드 중 2그리드 사용 -->'+
+'								<img alt="" src="/jspweb/admin/productimg/'+jsonarray[i]["pimg"]+'" width="70%"> '+
+'							</div>'+
+'							<div class="col-sm-10">'+
+'								<div class="pnamebox"> '+jsonarray[i]["pname"]+' </div>'+
+'								<div class="optionbox">'+jsonarray[i]["scolor"]+' / '+jsonarray[i]["ssize"]+'</div>'+
+'							</div>'+
+'						</div>   '+
+'						<br>'+
+'					</td> '+
+'					<td class="align-middle">'+
+'						<div class="row g-0"> '+
+'							<div class="col-sm-5 offset-3">'+ jsonarray[i]["samount"] +' 개 </div>'+
+'						</div>'+
+'					</td>'+
+'					<td class="align-middle">'+
+'						<div class="row g-0">'+
+'							<div class="col-md-8 pricebox">'+jsonarray[i]["totalprice"].toLocaleString()+'원</div>'+
+'						</div>'+
+'					</td>'+
+'				</tr>';
 			}
-		}
-	});
+			// 만약에 총가격이 7만원 이상이면 배송비 무료
+			if( sumprice >= 70000 ){ deliverypay = 0;}
+			else{ deliverypay = 2500; }
+			
+			// 만약에 장바구니에 상품이 없으면 
+			if( jsonarray.length == 0 ){
+				tr += '<td style="text-align: center" colspan="3">'+ 
+						'상품이 없습니다. '+
+						'</td>';
+				deliverypay = 0; 
+			}
+			// 총주문금액 = 총가격 + 배송비 
+			totalpay = sumprice + deliverypay;
+			// 포인트 
+			point = parseInt( sumprice * 0.01 ); /* js : parseInt( 데이터 ) : -> 정수형 변환 */
+			// 출력 
+			$("#carttable").html( tr );
 }
 /* 아임포트 API = 결제API */
 function payment(){
@@ -49,6 +130,31 @@ function saveorder(){
 		}
 	});
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- jspweb/src/main/webapp/product/orderpay.jsp
+++ jspweb/src/main/webapp/product/orderpay.jsp
@@ -15,17 +15,62 @@
 				<span> 01 옵션선택 </span> <span> > </span>
 				<span> 02 장바구니 </span> <span> > </span>
 				<span  id="process_active"> 03 주문결제 </span> <span> > </span>
-				<span> 04 주문완료 </span>
+				<span style="font-size: 1.0vw"> 04 주문완료 </span>
 			</div>	
-				
-			<div class="row my-5">
-				<div class="col-sm-3 offset-3">
-					<a href="/jspweb/main.jsp">
-						<button class="form-control py-4"> 계속 쇼핑하기 </button>
-					</a> 
+			
+			<div class="row">
+				<div class="col-sm-8">
+					<div> <!--  제품 목록  -->
+						<table class="table my-5" id="carttable">
+						</table>
+					</div>
+					
+					<div> <!--  받는 사람 정보 -->
+						<h5> 받는 사람</h5> 
+						<input type="checkbox" id="checkbox"> 회원과 동일 
+						<form>
+							성명 <input id="ordername"> <br>
+							연락처 <input id="orderphone"> <br>
+							주소 <input id="orderaddress"> <br>
+							배송 요청사항 <input id="orderrequest"> <br>
+						</form>
+					</div>
+					
 				</div>
-				<div class="col-sm-3">
-					<button onclick="payment()" style="background-color: blue; color: white;" class="form-control py-4"> 결제하기 </button>
+				
+				<div class="col-sm-4">
+					<div class="paybox">
+						<div> <!-- 포인트 사용 -->
+							* 5000포인트 이상부터 사용가능합니다.
+							포인트 : <input> <button>사용</button>
+						</div>
+						<div> <!-- 결제 정보 -->
+							<div>
+								총 상품금액 : <span> 29,000원 </span>  <br>
+								배송비 : <span> 3,000원 </span>  <br>
+								포인트사용 : <span> 0 원 </span> <br>
+								총 결제액 : <span> 32,000 원 </span> <br>
+							</div>
+							
+							<div> 결제 수단
+								<button> 삼성페이 </button>
+								<button> 카드 </button>
+								<button> 계좌이체 </button>
+								<button> 무통장 </button>
+								<button> 핸드폰 </button>
+							</div>
+							<div class="row">
+								<div class="col-sm-6">
+									<a href="/jspweb/main.jsp">
+										<button class="form-control  py-4"> 계속 쇼핑하기 </button>
+									</a> 
+								</div>
+								<div class="col-sm-6">
+									<button onclick="payment()" style="background-color: blue; color: white;" class="form-control py-4"> 결제하기 </button>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -33,5 +78,7 @@
 	<script type="text/javascript" src="https://cdn.iamport.kr/js/iamport.payment-1.2.0.js"></script>
 	<script type="text/javascript" src="/jspweb/js/orderpay.js"></script>
 	<%@include file = "../footer.jsp" %>
+	
+	
 </body>
 </html>
