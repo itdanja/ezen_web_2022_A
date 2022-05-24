8b7321febc0ee319b57bb598bdf7db9e0bfa9343
itdanja <itdanja@kakao.com>
2022-05-24 14:05:57 +0900

    day58 하위리스트 출력시 i -> j

--- jspweb/src/main/webapp/js/myshopping.js
+++ jspweb/src/main/webapp/js/myshopping.js
@@ -28,8 +28,8 @@ $(window).scroll( function(){
 		// $(window).scrollTop() : 스크롤막대의 상단 위치
 		// $(document).height() : 현재 보고 있는 문서내 높이 
 		// $(window).height() : 현재 보고 있는 화면 높이 
-	if( $(window).scrollTop() == $(document).height() - $(window).height()  ){
-		// 현재 스크롤의 상단 위치  ==  현재 보고 있는 문서내 높이   -  현재 보고 있는 화면 높이 
+	if( $(window).scrollTop() >= $(document).height() - $(window).height()  ){
+		// 현재 스크롤의 상단 위치  >=  현재 보고 있는 문서내 높이   -  현재 보고 있는 화면 높이 
 		viewcount++; // 스크롤 바닥에 닿을때 마다 출력하는 주문개수를 증가
 		view(); // 새로고침
 	}
@@ -53,9 +53,9 @@ function view(){
 			html += 
 				'<div class="row"> '+
 						'<div class="col-sm-8">'+
-							'<span> <img width="100%" alt="" src="/jspweb/admin/productimg/'+childlist[i]["pimg"]+'"></span>'+
-							'<span> '+childlist[i]["pname"]+' </span>'+
-							'<span> '+childlist[i]["scolor"]+'/'+childlist[i]["ssize"]+'</span>'+
+							'<span> <img width="100%" alt="" src="/jspweb/admin/productimg/'+childlist[j]["pimg"]+'"></span>'+
+							'<span> '+childlist[j]["pname"]+' </span>'+
+							'<span> '+childlist[j]["scolor"]+'/'+childlist[j]["ssize"]+'</span>'+
 						'</div>'+
 						'<div class="col-sm-4">'+
 							'<button> 배송조회 </button>'+
