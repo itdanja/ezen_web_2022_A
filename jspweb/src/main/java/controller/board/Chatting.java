ba2e28130c4314383641cd3278f021d5785471b0
itdanja <itdanja@kakao.com>
2022-05-27 12:12:27 +0900

    day61 기본 웹소켓 통신

--- jspweb/src/main/java/controller/board/Chatting.java
+++ jspweb/src/main/java/controller/board/Chatting.java
@@ -1,6 +1,9 @@
 package controller.board;
 
-import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.util.List;
+import java.util.Vector;
+
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
@@ -13,19 +16,28 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint("/chatting")
 public class Chatting {	// 서버소켓 사용할 클래스 
 	
+	// 0.접속자 명단 
+	public static List< Session > clients = new Vector<>(); // 동기화 [ 한명씩 처리 ]
 	// 1. 소켓 오픈
 	@OnOpen
-	public void OnOpen( Session session ) {
-		System.out.println(" 서버소켓에 유저가 들어왔습니다. : "+session.toString() );
+	public void OnOpen( Session session ) { 
+		System.out.println( session +" 님이 서버소켓 으로 들어왔습니다.");
+		clients.add(session); // 해당 세션를 접속 명단에 추가 
+		System.out.println(" 현재 들어온 세션들 : " + clients.toString() );
 	}
 	// 2. 소켓 닫기 
 	@OnClose
-	public void OnClose() {
-		
+	public void OnClose( Session session ) {
+		clients.remove( session );	// 해당 세션 제거 
 	}
-	// 3. 소켓 메시지 보내기 
+	// 3. 소켓 메시지 받는다 -> 모든 유저에게 보내기
 	@OnMessage
-	public void OnMessage() {
+	public void OnMessage( String msg , Session session) throws IOException {
+		System.out.println( session+" 가 "+ msg +" 보냄 ");
+		
+		for( Session s : clients ) { // 리스트에 존재하는 세션들에게 메시지 보내기
+			s.getBasicRemote().sendText(msg);
+		}
 		
 	}
 }
--- jspweb/src/main/webapp/js/chatting.js
+++ jspweb/src/main/webapp/js/chatting.js
@@ -2,29 +2,41 @@
 $("#incontent").focus(); // 스크립트가 실행될때 입력창에 커서 두기
 let websocket = null; // js 웹소켓 변수 
 
-/* 엔터 입력했을때 이벤트 */
-function enterkey(){
-	
-	if( window.event.keyCode == 13 ){
-		$("#incontent").val(""); // 전송후 입력창 초기회
-		$("#incontent").focus();	// 전송후 포커스(커서)
-	}
-	
-}
-/* 전송 버튼를 눌렀을때 이벤트 */
-function sendbtn(){
-	$("#incontent").val(""); // 전송후 입력창 초기회
-	$("#incontent").focus();	// 전송후 포커스(커서)
-}
+/* 1.  엔터 입력했을때 이벤트 */
+function enterkey(){    if( window.event.keyCode == 13 ){  send();  }  }
+/* 2. 전송 버튼를 눌렀을때 이벤트 */
+function sendbtn(){   send();  }
 
-
+start();
 /* 채팅 소켓 실행 */
 function start(){
 	//	js 에서 사용되는 웹소켓 클래스( ws:// IP번호/PORT번호/프로젝트명/서버소켓이름 )
 	websocket = new WebSocket("ws://localhost:8081/jspweb/chatting");
 	websocket.onopen = function( e ){ onOpen(e) }
+	websocket.onmessage = function( e ){ onmessage(e) }
 }
 function onOpen(){
-	alert("서버소켓으로 들어왔습니다. : "+ e );
+	alert("서버소켓으로 들어갑니다~ : "+ e );
 	console.log( e );
 }
+function onmessage( e ){
+	alert("메시지를 받습니다." + e +" 내용물 : " + e.data );
+	console.log( e );
+}
+function send(){
+	alert("메시지를 보냅니다.");
+	
+	let msg = $("#incontent").val();
+	
+	websocket.send(  msg  );
+	
+	$("#incontent").val(""); // 전송후 입력창 초기화
+	$("#incontent").focus();	// 전송후 포커스(커서)
+}
+
+
+
+
+
+
+
